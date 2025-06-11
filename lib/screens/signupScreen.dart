c86a5e180e7961e9b93659bdee2788183fe06bcc
FumiMelvinDev <fumimelvindev@gmail.com>
2025-06-11 20:18:51 +0200

    Implement signup functionality with email and password input

--- lib/screens/signupScreen.dart
+++ lib/screens/signupScreen.dart
@@ -1,3 +1,4 @@
+import 'package:ai_notes/auth/auth_service.dart';
 import 'package:flutter/material.dart';
 
 class Signupscreen extends StatefulWidget {
@@ -8,8 +9,44 @@ class Signupscreen extends StatefulWidget {
 }
 
 class _SignupscreenState extends State<Signupscreen> {
+  final authService = AuthService();
+
+  final _emailController = TextEditingController();
+  final _passwordController = TextEditingController();
+
+  void register() async {
+    final email = _emailController.text;
+    final password = _passwordController.text;
+
+    try {
+      await authService.signup(email, password);
+    } catch (e) {
+      if (mounted) {
+        ScaffoldMessenger.of(
+          context,
+        ).showSnackBar(SnackBar(content: Text('Sign up failed: $e')));
+      }
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: ListView(
+        children: [
+          TextField(controller: _emailController),
+          TextField(controller: _passwordController),
+          ElevatedButton(onPressed: register, child: Text('Sign Up')),
+          GestureDetector(
+            onTap: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => Signupscreen()),
+              );
+            },
+          ),
+        ],
+      ),
+    );
   }
 }
